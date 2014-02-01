c28daf8c90367261d1759202addc70ebd35e4f6d
akimaru506 <akatukimaru.rescue@gmail.com>
2014-02-01 23:10:53 +0900

    よくわからんが動かん

--- net/sample/welcome/WelcomeShow.java
+++ net/sample/welcome/WelcomeShow.java
@@ -9,10 +9,13 @@ import android.view.View;
 public class WelcomeShow extends View{
 		public WelcomeShow(Context context){
 			super(context);
-			setBackgroundColor(Color.WHITE);
+			setBackgroundColor(Color.BLUE);
 		}
 		protected void onDrow(Canvas canvas){
-			canvas.drawText("Welcome droid World!",0,12,new Paint());
+			Paint paint = new Paint();
+			paint.setColor(Color.WHITE);
+			//paint.setAlpha(TEXT_ALIGNMENT_CENTER);
+			canvas.drawText("Welcome droid World!",0,12,paint);
 		}
 
 }
