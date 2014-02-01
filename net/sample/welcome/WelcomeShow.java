4441f9e186c52e7d3b31900e17418c36fb6515aa
akimaru506 <akatukimaru.rescue@gmail.com>
2014-02-01 22:40:14 +0900

    a

---
+++ net/sample/welcome/WelcomeShow.java
@@ -0,0 +1,18 @@
+package net.sample.welcome;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+
+public class WelcomeShow extends View{
+		public WelcomeShow(Context context){
+			super(context);
+			setBackgroundColor(Color.WHITE);
+		}
+		protected void onDrow(Canvas canvas){
+			canvas.drawText("Welcome droid World!",0,12,new Paint());
+		}
+
+}
